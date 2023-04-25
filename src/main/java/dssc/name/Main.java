540a4d5544fce7a05c79e1362f4527290ed69cc8
vhonule <honule@esteco.com>
2023-04-25 19:05:34 +0530

    Initial commit

---
+++ src/main/java/dssc/name/Main.java
@@ -0,0 +1,12 @@
+package dssc.name;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello stranger!");
+        } else {
+            System.out.println("Hello " + args[0] + "!");
+        }
+    }
+}
